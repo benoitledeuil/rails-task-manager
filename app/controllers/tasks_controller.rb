5dfac4f8f3c8d89134ad77811301618571b7705b
Benoit LEDEUIL <benoitledeuil@gmail.com>
2024-05-22 11:03:11 +0200

    rails new

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path(@tasks)
+  end
+
+#   def checkboxes
+#     if @task.completed == true
+# return "This task is completed"
+#     else "This taks is not ccompleted yet!"
+#     end
+
+#   end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title, :details)
+    end
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+end
